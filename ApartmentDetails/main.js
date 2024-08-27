64fde348888c1e62c38ea8439ec0cadc48b2bd97
Ultimate Guru <tosindaniel360@gmail.com>
2024-08-27 13:10:55 +0100

    changed the getElement

--- ApartmentDetails/main.js
+++ ApartmentDetails/main.js
@@ -1,4 +1,12 @@
-const url = './data.json';
+const checkbox = document.getElementById('accountActivation');
+const Btn = document.getElementById('deactivateButton');
+
+// Toggle the button's disabled state based on checkbox status
+checkbox.addEventListener('click', () => {
+    Btn.disabled = !checkbox.checked;
+});
+
+const url = './assets/data.json';
 
 getData();
 
@@ -8,11 +16,26 @@ async function getData() {
         if (response.status === 200) {
             let data = await response.json();  // Await the promise from response.json()
             console.log(data);
+            return data;
         } else {
             console.log(response.ok);  // response.ok returns true or false
             console.log('Data not found');
+            return null;
         }
     } catch (error) {
         console.log('Error:', error);
+        return null;
     }
 }
+
+const showData = async () => {
+    const data = await getData();  // Await the async function
+    if (data) {
+        const dataElements = document.getElementById('strong');
+        dataElements.forEach((element, index) => {
+            element.innerHTML = data[index] || 'No data';  // Assign data to each strong element or display 'No data'
+        });
+    }
+};
+
+showData();
